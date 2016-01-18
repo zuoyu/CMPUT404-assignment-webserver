b140c2f6c7c9839e9f8fc9206e256b30dff62db2
zuoyu <zuo2@ualberta.ca>
2016-01-18 14:12:32 -0700

    Update server.py

--- server.py
+++ server.py
@@ -1,7 +1,7 @@
 #  coding: utf-8 
 import SocketServer
 
-# Copyright 2013 Abram Hindle, Eddie Antonio Santos
+# Copyright 2013 Abram Hindle, Eddie Antonio Santos,Yu Zuo, Qiang Yu
 # 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
